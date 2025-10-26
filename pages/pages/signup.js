9831d330c97768620d72c4caf10745fa03cda3f6
pnyanguwo-cyber <tilakerwin@gmail.com>
2025-10-26 19:06:37 +0200

    Create signup.js

---
+++ pages/pages/signup.js
@@ -0,0 +1,5 @@
+// Copy the login.js file and modify:
+// - Change "Welcome back" to "Create your account"
+// - Change "Sign In" to "Sign Up"
+// - Add "Confirm Password" field
+// - Change bottom link to point to /login
